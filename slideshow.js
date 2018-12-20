244c4ba6b5c7b9a618e7a01050d2f4e5e9b4c187
rikk626a <32450104+rikk626a@users.noreply.github.com>
2018-12-20 11:21:35 +0100

    nyeste push

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-
+// Next/previous controls
 function plusSlides(n) {
      showSlides(slideIndex += n);
 }
 
-
+// Thumbnail image controls
 function currentSlide(n) {
      showSlides(slideIndex = n);
 }
@@ -14,8 +14,7 @@ function currentSlide(n) {
 function showSlides(n) {
      var i;
      var slides = document.getElementsByClassName("mySlides");
-     var dots = document.getElementsByClassName("demo");
-     var captionText = document.getElementById("caption");
+     var dots = document.getElementsByClassName("dot");
      if (n > slides.length) {
           slideIndex = 1
      }
@@ -30,5 +29,4 @@ function showSlides(n) {
      }
      slides[slideIndex - 1].style.display = "block";
      dots[slideIndex - 1].className += " active";
-     captionText.innerHTML = dots[slideIndex - 1].alt;
 }
